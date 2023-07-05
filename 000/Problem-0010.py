855d0e2252d36502c7c312ce223e0937374e0f8f
Khair Alanam <anaambasheer@gmail.com>
2023-07-05 21:42:29 +0300

    Add function to check prime

---
+++ 000/Problem-0010.py
@@ -0,0 +1,34 @@
+
+
+
+from math import sqrt
+
+def is_prime(n: int) -> bool:
+    """
+    function: Check whether n is prime
+    params: n: int
+    returns: bool
+    """
+
+    # prime checking for number 1 - 10 and numbers and divisible by 2 and 3
+    if n == 1:
+        return False
+    if n < 4:
+        return True
+    if n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+
+    # Every prime number > 3 can be written as 6k - 1 or 6k + 1
+    n_sqrt = int(sqrt(n))
+    f = 5
+
+    while f <= n_sqrt:
+        if n % f == 0 or n % (f + 2) == 0:
+            return False
+        f += 6
+
+    return True
